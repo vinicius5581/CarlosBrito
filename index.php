4bfba39272afe1cfa98b91ca81c45dd84f6946ca
Vinicius Santana <santana@daterraweb.com>
2013-06-05 11:27:07 -0700

    1st files
    Deployment of files to get Carlos started.

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en" ><![endif]-->
+<!--[if gt IE 8]><!--> 
+	<html class="no-js" lang="en" >
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>TourQwest - San Francisco - CA</title>
+    <link rel="stylesheet" href="css/fontface.css" />
+    <link rel="stylesheet" href="-css/foundation.css" />
+    <link rel="stylesheet" href="css/style.css" />    
+</head>
+
+<body>
+    <div class="top-bar-bg"></div>
+    <div class="page-wrapper">
+    	<div class="top-bar-content"></div>        
+        <div class="columns-wrapper">
+        	<div class="main-left-column">
+            	<h2 class="main-left-list-header">Choose a TourQwest</h2>
+            	<ul class="main-left-list">
+                	<li><a href="#">Qwest San Francisco</a>
+                    	<ul>
+                    		<li><a href="#">Qwest San Francisco &amp; Alcatraz</a></li>
+                    	</ul>
+                    </li>
+                    <li><a href="#">Qwest Muir Woods &amp; Sausalito</a></li>
+                    <li><a href="#">Qwest San Francisco by Night</a></li>
+                    <li><a href="#">Qwest Saver by Day</a></li>
+                    <li><a href="#">Qwest Saver by Night</a></li>
+                    <li><a href="#">Qwest Wine Country</a></li>
+                    <li><a href="#">Qwest Monterey &amp; Carmel</a></li>
+                    <li><a href="#">Qwest Yosemite by Day</a></li>
+                </ul>
+			  <h2 class="main-left-list-header">Choose an Airport Qwest</h2>
+            	<ul class="main-left-list">
+                	<li><a href="#">San Francisco</a></li>
+                	<li><a href="#">Oakland</a></li>
+                </ul>
+                <ul class="main-left-social-media">
+                	<li><a href="#">Facebook</a></li>
+                	<li><a href="#">Yelp</a></li>
+                	<li><a href="#">Twitter</a></li>
+                	<li><a href="#">Google +</a></li>
+                	<li><a href="#">SM</a></li>
+                	<li><a href="#">Instagram</a></li>
+                	<li><a href="#">SM</a></li>
+                	<li><a href="#">Pinterest</a></li>
+                    
+                </ul>    
+            </div>
+            <div class="main-midle-column">
+                <div class="main-midle-header">
+                    <h3 id="gc-logo">Golden Compass</h3>
+                    <h1 id="main-logo"><a href="#">TourQwest - San Francisco - Bay Area - Tour Packages</a></h1>   
+                    <ul>
+                        <li><a href="#">Win Awesome Rewards!</a></li>
+                        <li><a href="#">Interactive Visitor's Map</a></li>
+                    </ul> 
+                </div>
+                <div class="main-midle-content">
+                    <p>Content</p>
+                </div>
+			</div>
+            <div class="main-right-column">
+            	<div class="shopping-cart">
+                	<h3>Shopping Cart</h3>
+                    <p>Buying(seats)0,$0<span>0</span></p>
+                    <p>Earning<span>0</span></p>
+                    <p>Credit<span>0</span></p>
+                    <p>Total 0 Tokens!</p>
+                    <p>Redeeming<span>0</span></p>
+                    <p>Balance<span>0</span></p>
+                    <a href="#">Check Out</a>                    
+                </div>
+                <h2 class="main-right-list-header">Choose Reward</h2>
+              <h2 class="main-right-list-header">15-20 Tokens</h2>
+            	<ul class="main-right-list">
+                	<li><a href="#">Ghirardelli : Ice Cream <br>
+               	    &amp; Chocolate</a></li>
+                    <li><a href="#">Pier 39 : 3-D Bay Theater</a></li>
+                    <li><a href="#">Starbucks: Gift Card</a></li>
+                    <li><a href="#">USS Balchuta</a></li>
+                    <li><a href="#">AMC Movie Admission</a></li>
+                    <li><a href="#">Coit Tower Lift Ticket</a></li>
+                    <li><a href="#">Japanese Tea Garden</a></li>
+                    <li><a href="#">Pier 39 : Turbo Ride - <br>
+                    7D Experience</a></li>
+                </ul>	
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</body>
+</html>
